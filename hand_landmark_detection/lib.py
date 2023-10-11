d94052935f764ff86a53dc2f97b8fe700c2efc9d
gwangmin <ygm.gwangmin@gmail.com>
2023-10-11 16:29:18 +0900

    update example funcs in hand_landmark_detection

--- hand_landmark_detection/lib.py
+++ hand_landmark_detection/lib.py
@@ -84,7 +84,7 @@ def draw_landmarks(img, hand_landmark_obj):
 
 def plot_world_landmarks(hand_world_landmark_obj):
     '''
-    (accuracy is very low)
+    (accuracy is low)
 
     Plot one hand landmarks in world coord.
 
@@ -184,36 +184,6 @@ def playground():
     '''
     Hand landmark detection test from webcam
     '''
-    hand_landmarker = HandLandmarker(max_num_hands=2,
-                                      min_detection_confidence=0.5,
-                                      min_tracking_confidence=0.5)
-    cam = cv2.VideoCapture(0)
-    cv2.namedWindow('webcam', cv2.WINDOW_AUTOSIZE) # this window size cannot change, automatically fit the img
-    while cam.isOpened():
-        success, frame = cam.read() # get frame from cam
-        if success:
-            # preprocessing
-            frame = cv2.flip(frame, 1) # y-axis flip
-            # get hand landmarks
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            result = hand_landmarker.get_result(img)
-            # extract info
-            if result.multi_hand_landmarks:
-                for hand_landmarks in result.multi_hand_landmarks:
-                    draw_landmarks(frame, hand_landmarks)
-            # show frame
-            cv2.imshow('webcam', frame)
-            if cv2.waitKey(delay=1) == ord('q'):
-                break
-    # exit
-    hand_landmarker.close()
-    cam.release()
-    cv2.destroyAllWindows()
-
-def example1():
-    '''
-    Example
-    '''
     hand_landmarker = HandLandmarker(max_num_hands=1,
                                       min_detection_confidence=0.5,
                                       min_tracking_confidence=0.5)
@@ -234,15 +204,18 @@ def example1():
                     index_tip = hand_landmarks.landmark[landmarks_num.INDEX_FINGER_TIP]
                     middle_tip = hand_landmarks.landmark[landmarks_num.MIDDLE_FINGER_TIP]
 
-                    thumb_tip_arr = np.array([thumb_tip.x, thumb_tip.y, thumb_tip.z])
-                    index_tip_arr = np.array([index_tip.x, index_tip.y, index_tip.z])
-                    middle_tip_arr = np.array([middle_tip.x, middle_tip.y, middle_tip.z])
-                    dist = np.linalg.norm(thumb_tip_arr - middle_tip_arr)
+                    text = f'THUMB_TIP: {int(thumb_tip.x*100)}, {int(thumb_tip.y*100)}, {int(thumb_tip.z*100)}'
+                    cv2.putText(frame, text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                (0,0,255), thickness=3, lineType=cv2.LINE_AA)
                     
-                    cv2.putText(frame, 
-                                f'thumb: {thumb_tip.x*100}, index: {index_tip.x*100}, dist: {dist*100}', 
-                                (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 
-                                thickness=3, lineType=cv2.LINE_AA)
+                    text = f'INDEX_FINGER_TIP: {int(index_tip.x*100)}, {int(index_tip.y*100)}, {int(index_tip.z*100)}'
+                    cv2.putText(frame, text, (10,60), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                (0,255,0), thickness=3, lineType=cv2.LINE_AA)
+                    
+                    text = f'MIDDLE_FINGER_TIP: {int(middle_tip.x*100)}, {int(middle_tip.y*100)}, {int(middle_tip.z*100)}'
+                    cv2.putText(frame, text, (10,90), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                (255,0,0), thickness=3, lineType=cv2.LINE_AA)
+                    
                     draw_landmarks(frame, hand_landmarks)
             # show frame
             cv2.imshow('webcam', frame)
@@ -255,4 +228,4 @@ def example1():
 
 
 if __name__ == '__main__':
-    example1()
+    playground()
