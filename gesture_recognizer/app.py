e9d01a7eff81f6b881f54d32590b52a3ad5854a1
gwangmin <ygm.gwangmin@gmail.com>
2023-11-28 16:15:36 +0900

    3nd app.py

--- gesture_recognizer/app.py
+++ gesture_recognizer/app.py
@@ -5,10 +5,12 @@ api for LDH game
 import cv2
 import numpy as np
 from lib import GestureRecognizer
+from lib import landmarks_num
 from lib import OneHandGestureManager
 
 from FingerSnap import FingerSnap
 from click import Click
+from fistAndOpen import FistAndOpen
 
 
 # Settings
@@ -24,13 +26,14 @@ timestamp_ms = 0
 # prepare
 fingersnap_mgr = OneHandGestureManager([FingerSnap(DISTANCE_THRESHOLD[FingerSnap])] * max_num_hands)
 click_mgr = OneHandGestureManager([Click(DISTANCE_THRESHOLD[Click])] * max_num_hands)
+fao_mgr = OneHandGestureManager([FistAndOpen()] * max_num_hands)
 
 
 # define gesture_id
 NONE = 0
-
+FIST_MOVE = 3
 id2mgr = {
-    NONE: 'None',
+    1: fao_mgr,
     2: click_mgr,
     4: fingersnap_mgr,
 }
@@ -49,6 +52,7 @@ def gesture_recognizer(webcam_bgr_img):
     return: tuple. (marked bgr image, list of recognized gesture).
             recognized gesture: (gesture_id, gesture_info)
     '''
+    global timestamp_ms
     # preprocessing
     webcam_bgr_img = cv2.flip(webcam_bgr_img, 1) # y-axis flip
     # get recognition result
@@ -62,20 +66,56 @@ def gesture_recognizer(webcam_bgr_img):
             handedness = result.handedness[i][0].display_name
             hand_landmarks = result.hand_landmarks[i]
             info = {'gesture_name': result.gestures[i][0].category_name}
-            ###
-            for i, (id,mgr) in enumerate(id2mgr.items()):
+            # fist_move
+            if info['gesture_name'] == GestureRecognizer.CLOSED_FIST:
+                index_tip = hand_landmarks[landmarks_num.INDEX_FINGER_TIP]
+                x = round(index_tip.x, 2)
+                y = round(index_tip.y, 2)
+                z = round(index_tip.z, 2)
+                gesture_info = {'handedness': handedness, 'xyz': (x, y, z)}
+                ret.append((FIST_MOVE, gesture_info))
+            # other gestures
+            for id, mgr in id2mgr.items():
                 if mgr.check(i, handedness, hand_landmarks, info):
                     gesture_info = {'handedness': handedness}
-                    ###
-                    # if isinstance(mgr.instance_list[0], GESTURE CLASS):
-                    #     add info
-                    ###
                     ret.append((id, gesture_info))
-                # check, handler
             GestureRecognizer.draw_landmarks(rgb, hand_landmarks)
     else:
         for id, mgr in id2mgr.items():
             mgr.init('all')
+    if not ret:
         ret.append((NONE, None))
+    timestamp_ms += 1 #
     bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
     return (bgr, ret)
+
+
+def test():
+    '''
+    func test from webcam
+    '''
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow('webcam', cv2.WINDOW_AUTOSIZE) # this window size cannot change, automatically fit the img
+
+    while cam.isOpened():
+        success, frame = cam.read() # get frame from cam
+        if success:
+            # func call
+            result = gesture_recognizer(frame)
+
+            text = f'{result[1]}'
+            cv2.putText(result[0], text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                (0,0,255), thickness=3, lineType=cv2.LINE_AA)
+            
+            # show frame
+            cv2.imshow('webcam', result[0])
+            if cv2.waitKey(delay=1) == ord('q'):
+                break
+    # exit
+    recognizer.close()
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    test()
